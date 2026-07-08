74b31e7ec574a84707e2a95562a31b66505b529e
ll6692717-bit <ll6692717@gmail.com>
2026-07-09 07:12:44 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'KTL COUNT｜奇天烈軍販売状況共有',
-  description: '奇天烈軍の雑誌販売状況共有サイト',
+  title: '🗝🔥販売状況共有',
+  description: '🗝🔥販売状況共有サイト',
 };
 
 export default function RootLayout({
